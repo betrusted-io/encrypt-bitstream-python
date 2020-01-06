df4392fa6f1449b89ee1e3a9969ee694a7b9f061
bunnie <bunnie@kosagi.com>
2020-01-07 01:31:32 +0800

    remove the unused .tcl file, it's confusing

--- bitstream.tcl
+++
@@ -1,8 +0,0 @@
-set_property CONFIG_VOLTAGE 1.8 [current_design]
-set_property CFGBVS GND [current_design]
-set_property BITSTREAM.CONFIG.CONFIGRATE 66 [current_design]
-set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 1 [current_design]
-set_property BITSTREAM.ENCRYPTION.ENCRYPT YES [current_design]
-set_property BITSTREAM.ENCRYPTION.ENCRYPTKEYSELECT eFUSE [current_design]
-set_property BITSTREAM.ENCRYPTION.KEY0 256'h0000000000000000000000000000000000000000000000000000000000000000 [current_design]
-write_bitstream -force top.bit 
